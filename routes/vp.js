e7164a855324f75eb9cce8ed35cafd0e1da42f5b
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-27 20:25:03 +0700

    Update vp.js

--- routes/vp.js
+++ routes/vp.js
@@ -8,24 +8,25 @@ router.get('/search', async (req, res) => {
   const query = req.query.query
   if (!query)
     return res.json({
-      creator: 'Guru sensei',
+      creator: 'galihrhgnwn',
       status: false,
       msg: 'query is required',
+      usage: 'type /docs at the end of the link (https://api.galihjsdev.xyz/docssdev.xyz/docs)'
     })
   const result = await vadapav.search(query)
-  res.json({ creator: 'Guru sensei', status: true, msg: result })
+  res.json({ creator: 'galihrhgnwn', status: true, msg: result })
 })
 
 router.get('/download', async (req, res) => {
   const id = req.query.id
   if (!id)
     return res.json({
-      creator: 'Guru sensei',
+      creator: 'galihrhgnwn',
       status: false,
       msg: 'id is required',
     })
   const result = await vadapav.download(id)
-  res.json({ creator: 'Guru sensei', status: true, msg: result })
+  res.json({ creator: 'galihrhgnwn', status: true, msg: result })
 })
 
 export default router
