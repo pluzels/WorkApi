49f04c60d41ba588b31b603688e7bd2368294934
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-28 13:54:54 +0700

    Update xnxx-search.js

--- routes/xnxx-search.js
+++ routes/xnxx-search.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   const query = req.query.query
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' })
+  if (!query) return res.json({ creator: 'galihrhgnwn', status: false, msg: 'query is required' })
   const result = await xnxxSearch(query)
   res.json(result)
 })
