ac6062812cadb3b7033fd2005ebf352adb719ca9
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-28 12:46:06 +0700

    Update ytdl.js

--- routes/ytdl.js
+++ routes/ytdl.js
@@ -10,35 +10,35 @@ const router = express.Router()
 
 router.get('/ytmp3', async (req, res) => {
   const url = req.query.url
-  if (!url) return res.json({ creator: 'Guru sensei', status: false, msg: 'URL is required' })
+  if (!url) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'URL is required' })
   const search = await ytdl.ytaudio(url)
   res.json(search)
 })
 
 router.get('/ytmp4', async (req, res) => {
   const url = req.query.url
-  if (!url) return res.json({ creator: 'Guru sensei', status: false, msg: 'URL is required' })
+  if (!url) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'URL is required' })
   const search = await ytdl.ytvideo(url)
   res.json(search)
 })
 
 router.get('/ytsearch', async (req, res) => {
   const query = req.query.query
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' })
+  if (!query) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'query is required' })
   const result = await ytsearch(query)
   res.json(result)
 })
 
 router.get('/v2/ytmp3', async (req, res) => {
   const url = req.query.url
-  if (!url) return res.json({ creator: 'Guru sensei', status: false, msg: 'URL is required' })
+  if (!url) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'URL is required' })
   const search = await ytdl2.ytaud(url)
   res.json(search)
 });
 
 router.get('/v2/ytmp4', async (req, res) => {
   const url = req.query.url
-  if (!url) return res.json({ creator: 'Guru sensei', status: false, msg: 'URL is required' })
+  if (!url) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'URL is required' })
   const search = await ytdl2.ytvid(url)
   res.json(search)
 });
