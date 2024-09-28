408997dbf6fba794be81a000784ff0671b9f8125
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-28 13:59:47 +0700

    Update playstore.js

--- routes/playstore.js
+++ routes/playstore.js
@@ -5,7 +5,7 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   const query = req.query.query
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' })
+  if (!query) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'query is required' })
   const result = await PlayStore(query)
   res.json(result)
 })
