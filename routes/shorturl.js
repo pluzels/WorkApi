979f1c471bce61d9be3a3286883b6ad3444b4176
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-28 14:01:19 +0700

    Update shorturl.js

--- routes/shorturl.js
+++ routes/shorturl.js
@@ -5,16 +5,16 @@ const router = express.Router()
 
 router.get('/bitly', async (req, res) => {
   const url = req.query.url
-  if (!url) return res.json({ creator: 'Guru sensei', status: false, msg: 'url is required' })
+  if (!url) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'url is required' })
   const result = await bitly(url)
-  res.json({ creator: 'Guru sensei', status: true, result })
+  res.json({ creator: 'galihrhgnwnn', status: true, result })
 })
 
 router.get('/tinyurl', async (req, res) => {
   const url = req.query.url
-  if (!url) return res.json({ creator: 'Guru sensei', status: false, msg: 'url is required' })
+  if (!url) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'url is required' })
   const result = await tinyurl(url)
-  res.json({ creator: 'Guru sensei', status: true, result })
+  res.json({ creator: 'galihrhgnwnn', status: true, result })
 })
 
 export default router
