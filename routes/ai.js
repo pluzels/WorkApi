b9fc5a545c499cad45e1069aee0ec29f432c42d9
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-28 13:55:47 +0700

    Update ai.js

--- routes/ai.js
+++ routes/ai.js
@@ -16,53 +16,53 @@ router.get('/bing', async (req, res) => {
   let username = req.query.username
   let query = req.query.query
   if (!username)
-    return res.json({ creator: 'Guru sensei', status: false, msg: 'username is required' })
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' })
+    return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'username is required' })
+  if (!query) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'query is required' })
   const result = await bing(username, query)
-  res.json({ creator: 'Guru sensei', status: true, msg: result })
+  res.json({ creator: 'galihrhgnwnn', status: true, msg: result })
 })
 
 router.get('/gpt4', async (req, res) => {
   let username = req.query.username
   let query = req.query.query
   if (!username)
-    return res.json({ creator: 'Guru sensei', status: false, msg: 'username is required' })
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' })
+    return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'username is required' })
+  if (!query) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'query is required' })
   const result = await gpt4(username, query)
-  res.json({ creator: 'Guru sensei', status: true, msg: result })
+  res.json({ creator: 'galihrhgnwnn', status: true, msg: result })
 })
 
 router.get('/blackbox', async (req, res) => {
   let query = req.query.query
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' })
+  if (!query) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'query is required' })
   const result = await blackbox(query)
-  res.json({ creator: 'Guru sensei', status: true, msg: result })
+  res.json({ creator: 'galihrhgnwnn', status: true, msg: result })
 })
 
 router.get('/darkgpt', async (req, res) => {
   let query = req.query.query
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' })
+  if (!query) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'query is required' })
   const result = await darky.Chat(query)
-  res.json({ creator: 'Guru sensei', status: true, msg: result })
+  res.json({ creator: 'galihrhgnwnn', status: true, msg: result })
 })
 
 router.get('/search', async (req, res) => {
   let query = req.query.query
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' })
+  if (!query) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'query is required' })
   const result = await aisearch.ask(query)
-  res.json({ creator: 'Guru sensei', status: true, msg: result })
+  res.json({ creator: 'galihrhgnwnn', status: true, msg: result })
 })
 
 router.get('/morphic', async (req, res) => {
   let query = req.query.query
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' })
+  if (!query) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'query is required' })
   const result = await morphic(query)
-  res.json({ creator: 'Guru sensei', status: true, msg: result })
+  res.json({ creator: 'galihrhgnwnn', status: true, msg: result })
 })
 
 router.get('/imagine', async (req, res) => {
   let query = req.query.query;
-  if (!query) return res.json({ creator: 'Guru sensei', status: false, msg: 'query is required' });
+  if (!query) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'query is required' });
 
   try {
       let imageUrl = imagine(query);
@@ -76,7 +76,7 @@ router.get('/imagine', async (req, res) => {
       response.body.pipe(res);
   } catch (error) {
       console.error(`Error fetching image: ${error.message}`); // Log any errors
-      res.status(500).json({ creator: 'Guru sensei', status: false, msg: error.message });
+      res.status(500).json({ creator: 'galihrhgnwnn', status: false, msg: error.message });
   }
 });
 
