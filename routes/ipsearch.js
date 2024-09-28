1b287f271ba6b563b2afc32dcd7efa1a7ee81eaf
pluzels <140618835+pluzels@users.noreply.github.com>
2024-09-28 13:58:50 +0700

    Update ipsearch.js

--- routes/ipsearch.js
+++ routes/ipsearch.js
@@ -5,12 +5,12 @@ const router = express.Router()
 
 router.get('/', async (req, res) => {
   const ip = req.query.ip
-  if (!ip) return res.json({ creator: 'Guru sensei', status: false, msg: 'ip is required' })
+  if (!ip) return res.json({ creator: 'galihrhgnwnn', status: false, msg: 'ip is required' })
   try {
     const result = await getipdetails(ip)
     res.json({ creator: 'Guru sensei', result })
   } catch (error) {
-    res.status(500).json({ creator: 'Guru sensei', status: false, msg: 'Internal Server Error' })
+    res.status(500).json({ creator: 'galihrhgnwnn', status: false, msg: 'Internal Server Error' })
   }
 })
 
